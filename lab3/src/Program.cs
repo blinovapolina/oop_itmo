fd2b2183c34c7f8b346531ef0d6d288d2aac39d0
blinovapolina <blinovapolina06@yandex.ru>
2025-12-04 21:00:24 +0300

    Modified Program.cs

--- lab3/src/Program.cs
+++ lab3/src/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using DeliverySystem.Core;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            var demo = new DeliverySystemDemo();
+            demo.Run();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
